cd1676227153f09adfc04715783fa4aa1be12d9e
arianf30 <arianfernandez@gmail.com>
2023-01-31 12:48:54 -0300

    fix: test aws 5

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default function () {
   return <>
-    <h1>Hi there! new 3</h1>
+    <h1>Hi there! new 5</h1>
     <hr />
     <MarketingApp />
   </>
